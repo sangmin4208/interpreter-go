30264498e42aab6bdd69e4dd34ecd5f239a9970c
SangMin <sangmin4208@gmail.com>
2022-05-11 18:09:27 +0900

    ✍️ Token, Lexer 기본 구조 작성

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sangmin4208/interpreter-go
+
+go 1.17
---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL" // 어떤 토큰이나 문자를 렉서가 알 수 없을 때 부여함
+	EOF     = "EOF"     // 파일의 끝을 의미
+
+	// 식별자 + 리터럴
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 123456
+
+	// 연산자
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// 구분자
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// 예약어
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
